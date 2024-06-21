cf22988f91be332225e71cfc2c0ed2a5385780ef
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-06-21 13:16:14 +0530

    Time: 13 ms (51.70%) | Memory: 7.7 MB (37.71%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    bool check(int i, int j, int k, vector<vector<char>>& board){
-        auto temp = k+'0';
-        for(int ind=0;ind<9;ind++){
-            if(board[ind][j]==temp){
-                return false;
-            }
-
-            if(board[i][ind]==temp){
+    // Check if it's valid to place 'num' at board[i][j]
+    bool check(int i, int j, char num, vector<vector<char>>& board) {
+        for (int ind = 0; ind < 9; ind++) {
+            // Check the row and column
+            if (board[ind][j] == num || board[i][ind] == num) {
                 return false;
             }
         }
-        
-        int tempi = 3*(i/3), tempj = 3*(j/3);
-        for(int ind=tempi;ind<tempi+3;ind++){
-            for(int jnd=tempj;jnd<tempj+3;jnd++){
-                if(board[ind][jnd]==temp){
+
+        // Correctly calculate the starting indices of the 3x3 box
+        int boxRowStart = (i / 3) * 3;  // CHANGED: Corrected box row calculation
+        int boxColStart = (j / 3) * 3;  // CHANGED: Corrected box column calculation
+        for (int row = boxRowStart; row < boxRowStart + 3; row++) {
+            for (int col = boxColStart; col < boxColStart + 3; col++) {
+                if (board[row][col] == num) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 
-    bool helper(vector<vector<char>>& board){
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
-                    for(int k=1;k<=9;k++){
-                        if(check(i,j,k,board)){
-                            board[i][j]='0'+k;
-                            if(helper(board)){
+    // Recursive function to solve the Sudoku board
+    bool helper(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    // Try placing numbers '1' to '9'
+                    for (char num = '1'; num <= '9'; num++) {  // CHANGED: Using char '1' to '9'
+                        if (check(i, j, num, board)) {
+                            board[i][j] = num;
+                            if (helper(board)) {
                                 return true;
+                            } else {
+                                board[i][j] = '.';  // Backtrack if not solvable
                             }
-                            else
-                            board[i][j]='.';
                         }
                     }
-                    return false;
+                    return false; // If no number can be placed, backtrack
                 }
             }
         }
-        return true;
+        return true; // If the entire board is filled correctly
     }
 
+    // Entry function to initiate the solving process
     void solveSudoku(vector<vector<char>>& board) {
         helper(board);
     }
