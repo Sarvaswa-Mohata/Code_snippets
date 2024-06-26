0a109575e58166ece3b6026e096bb9ea9f67ba56
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-06-26 14:44:57 +0530

    Time: 53 ms (90.73%) | Memory: 27.1 MB (93.82%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,26 +3,27 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<vector<int>> tmp;
-        int A=INT_MIN;
+        vector<vector<int>> v;
+        int A = INT_MIN;
         for(int i=0;i<n-2;i++){
             if(nums[i]==A){continue;}
             A=nums[i];
             int k=n-1;
             int j=i+1;
-            int a=INT_MIN, b= INT_MIN;
+            int a=INT_MIN, b=INT_MIN;
             while(j<k){
-                if(b==nums[j]){j++;continue;}
                 if(a==nums[k]){k--;continue;}
-                if(A+nums[j]+nums[k]==0){
-                    tmp.push_back({nums[i],nums[j],nums[k]});
+                if(b==nums[j]){j++;continue;}
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    v.push_back({nums[i],nums[j],nums[k]});
                     a=nums[k];
                     b=nums[j];
                     j++;
                     k--;
                 }
                 else{
-                    if(nums[i]+nums[j]+nums[k]>0){
+                    if(sum>0){
                         a=nums[k];
                         k--;
                     }
@@ -33,6 +34,6 @@ public:
                 }
             }
         }
-        return tmp;
+        return v;
     }
 };
