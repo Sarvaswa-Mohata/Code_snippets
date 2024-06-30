f53563d8d562138a0440d78a5f2a67132a22571f
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-06-30 14:04:15 +0530

    Time: 4 ms (14.20%) | Memory: 8.4 MB (67.52%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -11,31 +11,17 @@ public:
     }
     
     int pop() {
-        if(!op.empty()){
-            auto it = op.top();
-            op.pop();
-            return it;
-        }
-        int it;
-        while(!ip.empty()){
-            it = ip.top();
-            op.push(it);
-            ip.pop();
-        }
-        it=op.top();
-        op.pop();
-        return it;
+       auto it = peek();
+       op.pop();
+       return it;
     }
     
     int peek() {
-        if(!op.empty()){
-            return op.top();
-        }
-        int it;
-        while(!ip.empty()){
-            it=ip.top();
-            op.push(it);
-            ip.pop();
+        if(op.empty()){
+            while(!ip.empty()){
+                op.push(ip.top());
+                ip.pop();
+            }
         }
         return op.top();
     }
@@ -44,12 +30,3 @@ public:
         return op.empty() && ip.empty();
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
