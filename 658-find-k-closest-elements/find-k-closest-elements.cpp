2b2ec11872338b084567b69d44afb09525da948f
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-11 03:51:57 +0530

    Time: 25 ms (83.14%) | Memory: 34.2 MB (73.21%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n=arr.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
+        int low=0, high=n-k-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         vector<int> v;
-        int tmp = min(k,mp[x]);
-        k-=tmp;
-        while(tmp--){
-            v.push_back(x);
+        for(int i=low;i<low+k;i++){
+            v.push_back(arr[i]);
         }
-        for(int i=1;i<=20001;i++){
-            tmp = min(mp[x-i],k);
-            k-=tmp;
-            while(tmp--){
-                v.push_back(x-i);
-            }
-            tmp = min(mp[x+i],k);
-            k-=tmp;
-            while(tmp--){
-                v.push_back(x+i);
-            }
-        }
-        sort(v.begin(),v.end());
         return v;
     }
 };
