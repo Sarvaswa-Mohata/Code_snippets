cf065fe50209be4f7849cfaa6373804001d09a19
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-11 17:36:51 +0530

    Time: 225 ms (5.07%) | Memory: 43.1 MB (25.03%) - LeetSync

---
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int ans=1;
+        if(arr.size()==1){return ans;}
+        int l=0, r=0;
+        int n=arr.size();
+        while(r<n){
+            if(r<2){
+                if(r!=0){
+                    if(arr[r]==arr[r-1])
+                    l=r;
+                    else
+                    ans = max(ans,2);
+                }
+                r++;
+                continue;
+            }
+            if(arr[r]==arr[r-1]){
+                l=r;
+            }
+            else if(arr[r-1] == arr[r-2] || (arr[r]-arr[r-1])/abs(arr[r]-arr[r-1]) == (arr[r-1]-arr[r-2])/abs(arr[r-1]-arr[r-2])){
+                l=r-1;
+            }
+            ans = max(ans,r-l+1);
+            cout<<r<<","<<l<<" ";
+            r++;
+        }
+        return ans;
+    }
+};
