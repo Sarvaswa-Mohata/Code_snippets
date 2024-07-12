35d35ffc755a5a15d3131cdc65f62db58a662891
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-12 21:31:21 +0530

    Time: 28 ms (43.98%) | Memory: 259.1 MB (5.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int func(int i, int target, vector<int>& nums, vector<vector<int>>& dp){
         if(i<0){
-            return (target==1000);
+            return (target==10000);
         }
         if(dp[i][target]!=-1){
             return dp[i][target];
         }
-        return dp[i][target] = (target>=nums[i] ? func(i-1, target-nums[i], nums, dp) : 0) +(target+nums[i]<=2000 ? func(i-1, target+nums[i], nums, dp) : 0);
+        return dp[i][target] = func(i-1, target-nums[i], nums, dp) + func(i-1, target+nums[i], nums, dp);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        target=1000+target;
-        vector<vector<int>> dp(n, vector<int>(2001, -1));
+        target=10000+target;
+        vector<vector<int>> dp(n, vector<int>(20000, -1));
         return func(n-1, target, nums, dp);
     }
 };
