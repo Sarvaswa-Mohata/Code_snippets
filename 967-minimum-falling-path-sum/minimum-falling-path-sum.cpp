1cc2fa07a89a70fa893ceca2129d5f53f2984fb7
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-12 17:12:11 +0530

    Time: 17 ms (11.65%) | Memory: 13.7 MB (25.61%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,17 +4,19 @@ public:
         int n = matrix.size();
         int m = matrix[n-1].size();
         int ans = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(m,INT_MIN));
+        vector<int> prev(m,INT_MIN);
         for(int i=0;i<n;i++){
+            vector<int> curr(m,INT_MIN);
             for(int j=0;j<m;j++){
                 if(i==0)
-                dp[i][j]=matrix[i][j];
+                curr[j]=matrix[i][j];
                 else
-                dp[i][j] = matrix[i][j]+min({j>0 ? dp[i-1][j-1] : INT_MAX, dp[i-1][j], j<m-1 ? dp[i-1][j+1] : INT_MAX});
-                if(i==n-1){
-                    ans = min(ans,dp[i][j]);
-                }
+                curr[j] = matrix[i][j]+min({j>0 ? prev[j-1] : INT_MAX, prev[j], j<m-1 ? prev[j+1] : INT_MAX});
             }
+            prev = curr;
+        }
+        for(auto it:prev){
+            ans = min(ans, it);
         }
         return ans;
     }
