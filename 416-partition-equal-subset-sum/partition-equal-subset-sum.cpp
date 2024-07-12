7ca42a276aad3f5d417c4235d837c64c1ba2064f
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-13 01:31:36 +0530

    Time: 266 ms (32.62%) | Memory: 16.5 MB (60.06%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -5,15 +5,17 @@ public:
         auto sum= accumulate(nums.begin(),nums.end(),0);
         if(sum%2==1){return false;}
         int n=nums.size();
-        vector<vector<bool>> dp(n, vector<bool>(sum+1,false));
+        vector<bool> prev(sum+1, false);
         sum/=2;
-        dp[0][0] = true;
-        dp[0][nums[0]] = true;
+        prev[0] = true;
+        prev[nums[0]] = true;
         for(int i=1;i<n;i++){
+            vector<bool> curr(sum+1,false);
             for(int j=0;j<=sum;j++){
-                dp[i][j] =  (j>=nums[i]?dp[i-1][j-nums[i]]:false) | dp[i-1][j];
+                curr[j] =  (j>=nums[i]?prev[j-nums[i]]:false) | prev[j];
             }
+            prev = curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
