81d8bed05c646da9cea5fbdf966836e047c40fd2
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-12 14:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (14.55%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -18,6 +18,12 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n,-1));
-        return helper(m-1, n-1, grid, dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){ dp[i][j] = grid[i][j]; continue;}
+                dp[i][j] = grid[i][j]+min((i>0 ? dp[i-1][j] : INT_MAX),(j>0 ? dp[i][j-1] : INT_MAX));
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
