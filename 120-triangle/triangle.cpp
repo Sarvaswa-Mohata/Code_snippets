c9535d52612a8b20b00c21588b0de2bd27e14654
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-12 15:44:39 +0530

    Time: 5 ms (39.12%) | Memory: 11.6 MB (5.44%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -16,19 +16,22 @@ public:
         int n=triangle.size();
         int m=triangle[n-1].size();
         int ans = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(m,INT_MAX));
+        vector<int> prev(m,INT_MAX);
         if(n==1){
             return triangle[0][0];
         }
         for(int i=0;i<n;i++){
+            vector<int> curr(m,INT_MAX);
             for(int j=0;j<m;j++){
                 if(j>i) continue;
-                if(i==0 && j==0){dp[i][j] = triangle[i][j]; continue;}
-                dp[i][j] = triangle[i][j]+min((i>0 ? dp[i-1][j] : INT_MAX), (i>0 && j>0 ? dp[i-1][j-1] : INT_MAX));
-                if(i==n-1){
-                    ans = min(ans, dp[i][j]);
-                }
+                if(i==0 && j==0){curr[j] = triangle[i][j]; continue;}
+                curr[j] = triangle[i][j]+min((i>0 ? prev[j] : INT_MAX), (i>0 && j>0 ? prev[j-1] : INT_MAX));
             }
+            prev = curr;
+        }
+        for(auto it:prev){
+            cout<<it<<" ";
+            ans = min(ans,it);
         }
         return ans;
     }
