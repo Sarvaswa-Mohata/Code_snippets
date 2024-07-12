0bb74151acc072f2f8b9219222feb1ca89e3f409
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-12 15:35:49 +0530

    Time: 3 ms (83.01%) | Memory: 11.7 MB (5.44%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -16,10 +16,19 @@ public:
         int n=triangle.size();
         int m=triangle[n-1].size();
         int ans = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        for(int i=0;i<m;i++){
-            auto tmp = helper(n-1, i, triangle, dp);
-            ans = min(ans, tmp);
+        vector<vector<int>> dp(n, vector<int>(m,INT_MAX));
+        if(n==1){
+            return triangle[0][0];
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j>i) continue;
+                if(i==0 && j==0){dp[i][j] = triangle[i][j]; continue;}
+                dp[i][j] = triangle[i][j]+min((i>0 ? dp[i-1][j] : INT_MAX), (i>0 && j>0 ? dp[i-1][j-1] : INT_MAX));
+                if(i==n-1){
+                    ans = min(ans, dp[i][j]);
+                }
+            }
         }
         return ans;
     }
