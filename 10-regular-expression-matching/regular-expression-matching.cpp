987a3f55872d0dd74c6fbc4534944948da0cfce5
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-18 02:00:01 +0530

    Time: 463 ms (5.04%) | Memory: 8.9 MB (43.67%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool helper(int ind1, int ind2, string& s, string& p, int n, int m){
+    bool helper(int ind1, int ind2, string& s, string& p, int n, int m, vector<vector<int>>& dp){
         if(ind1>=n){
             for(int i=ind2;i<m;i++){
                 if(p[i]=='*' || p[i+1]=='*') continue;
@@ -12,26 +12,29 @@ public:
             if(ind1>=n) return true;
             return false;
         }
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
         if(p[ind2]!='.' && p[ind2]!='*'){
             if(s[ind1]!=p[ind2] && (ind2+1==m || p[ind2+1]!='*')){
-                return false;
+                return dp[ind1][ind2]=0;
             }
             if(p[ind2+1]=='*')
-            return helper(ind1, ind2+1, s, p, n, m);
-            return helper(ind1+1, ind2+1, s, p, n, m);
+            return helper(ind1, ind2+1, s, p, n, m, dp);
+            return helper(ind1+1, ind2+1, s, p, n, m, dp);
         }
         if(p[ind2]=='.'){
             if(ind2+1<m && p[ind2+1]=='*'){
-                return helper(ind1, ind2+1, s, p, n, m);
+                return helper(ind1, ind2+1, s, p, n, m, dp);
             }
-            return helper(ind1+1, ind2+1, s, p, n, m);
+            return helper(ind1+1, ind2+1, s, p, n, m, dp);
         }
         if(p[ind2]=='*'){
             bool ans = false;
-            ans|= helper(ind1, ind2+1, s, p, n, m); //matching 0 chars
+            ans|= helper(ind1, ind2+1, s, p, n, m, dp); //matching 0 chars
             if(p[ind2-1]=='.'){
                 while(ind1<n){
-                    ans|=helper(ind1+1, ind2+1, s, p, n, m);
+                    ans|=helper(ind1+1, ind2+1, s, p, n, m, dp);
                     ind1++;
                 }
             }
@@ -39,14 +42,15 @@ public:
                 auto it = p[ind2-1];
                 while(ind1<n && s[ind1]==it){
                     ind1++;
-                    ans|=helper(ind1, ind2+1, s, p, n, m); //matching 1 or more chars
+                    ans|=helper(ind1, ind2+1, s, p, n, m, dp); //matching 1 or more chars
                 }
             }
-            return ans;
+            return dp[ind1][ind2] = ans;
         }
         return true;
     }
     bool isMatch(string s, string p) {
-        return helper(0,0,s,p,s.size(),p.size());
+        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
+        return helper(0,0,s,p,s.size(),p.size(),dp);
     }
 };
