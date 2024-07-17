ec7ec6557731172c30504816b5f160f0eb5e928b
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-18 03:05:24 +0530

    Time: 42 ms (48.50%) | Memory: 30.1 MB (46.81%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        
-        vector<int> prev(m + 1, 0);
-        prev[m] = 1; // Base case: an empty pattern matches an empty string
-        
-        // Initialize for cases where the pattern contains only '*' characters
-        for (int j = m - 1; j >= 0; j--) {
-            if (p[j] == '*') prev[j] = prev[j + 1];
+        int n=s.size();
+        int m=p.size();
+        vector<int> prev(m+1, 0);
+        prev[m] = 1;
+        for(int j=m-1;j>=0;j--){
+            if(p[j]=='*') prev[j]=prev[j+1];
             else break;
         }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            vector<int> curr(m + 1, 0);
-            for (int j = m - 1; j >= 0; j--) {
-                if (p[j] == '*') {
-                    curr[j] = curr[j + 1] || prev[j]; // '*' can match empty or any sequence
-                } else if (p[j] == '?' || p[j] == s[i]) {
-                    curr[j] = prev[j + 1]; // Match one character or '?'
-                } else {
-                    curr[j] = 0; // No match
+        for(int i=n-1;i>=0;i--){
+            vector<int> curr(m+1, 0);
+            for(int j=m-1;j>=0;j--){
+                if(p[j]!='*' && p[j]!='?'){
+                    if(p[j]==s[i])
+                        curr[j] = prev[j+1];
+                    else
+                        curr[j] = 0;
+                }
+                else if(p[j]=='?'){
+                    curr[j] = prev[j+1];
+                }
+                else{
+                    curr[j]= curr[j+1] || prev[j];
                 }
             }
             prev = curr;
         }
-        
-        return prev[0];
+        return prev[0];  
     }
 };
