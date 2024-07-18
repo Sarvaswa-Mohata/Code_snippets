40cfe9be426095ab9dd33a7926ff891c2b5e0b40
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-18 12:54:16 +0530

    Time: 10 ms (40.87%) | Memory: 13.6 MB (18.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int helper(int ind1, int ind2, string& w1, string& w2, int n, int m, vector<vector<int>>& dp){
-        if(ind1>n || ind2>m){return 1e8;}
-        if(ind1==n && ind2==m){return 0;}
+    int helper(int ind1, int ind2, string& w1, string& w2, vector<vector<int>>& dp){
+        if(ind1<0) return ind2+1;
+        if(ind2<0) return ind1+1;
         if(dp[ind1][ind2]!=-1){
             return dp[ind1][ind2];
         }
-        if(ind1<n && ind2<m && w1[ind1]==w2[ind2]){
-            return dp[ind1][ind2] = helper(ind1+1, ind2+1, w1, w2, n, m, dp);
+        if(w1[ind1]==w2[ind2]){
+            return dp[ind1][ind2] = helper(ind1-1, ind2-1, w1, w2, dp);
         }
-        return dp[ind1][ind2] = min({1+helper(ind1, ind2+1, w1, w2, n, m, dp), 1+helper(ind1+1, ind2, w1, w2, n, m, dp), 1+helper(ind1+1, ind2+1, w1, w2, n, m, dp)});
+        return dp[ind1][ind2] = min({1+helper(ind1, ind2-1, w1, w2, dp), 1+helper(ind1-1, ind2, w1, w2, dp), 1+helper(ind1-1, ind2-1, w1, w2, dp)});
     }
     int minDistance(string w1, string w2) {
         int n=w1.size();
         int m=w2.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return helper(0, 0, w1, w2, n, m, dp);
+        return helper(n-1, m-1, w1, w2, dp);
     }
 };
