b137f9a193c3ad2e31d5f12d18a23f4934a37aa1
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-23 08:11:40 +0530

    Time: 16 ms (14.46%) | Memory: 20.9 MB (6.93%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -1,30 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<TreeNode*> generateTrees(int start, int end) {
-        vector<TreeNode*> result;
-        if (start > end) {
-            result.push_back(nullptr);
-            return result;
+    vector<TreeNode*> helper(int start, int end){
+        vector<TreeNode*> v;
+        if(start>end){
+            v.push_back(nullptr);
+            return v;
         }
-
-        for (int i = start; i <= end; i++) {
-            vector<TreeNode*> leftSubtrees = generateTrees(start, i - 1);
-            vector<TreeNode*> rightSubtrees = generateTrees(i + 1, end);
-
-            for (TreeNode* left : leftSubtrees) {
-                for (TreeNode* right : rightSubtrees) {
-                    TreeNode* root = new TreeNode(i);
-                    root->left = left;
-                    root->right = right;
-                    result.push_back(root);
+        for(int i=start; i<=end;i++){
+            for(auto it:helper(start, i-1)){
+                for(auto jt:helper(i+1, end)){
+                    TreeNode* node = new TreeNode(i);
+                    node->right = jt;
+                    node->left = it;
+                    v.push_back(node);
                 }
             }
         }
-        return result;
+        return v;
     }
-
     vector<TreeNode*> generateTrees(int n) {
-        if (n == 0) return {};
-        return generateTrees(1, n);
+        if(n==0) return {};
+        return helper(1,n);
     }
 };
