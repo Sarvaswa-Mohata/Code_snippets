d4e4d9eada53c76ef8c3025f7b19c8becad984cb
Sarvaswa Mohata <99800509+Sarvaswa-Mohata@users.noreply.github.com>
2024-07-23 08:15:06 +0530

    Time: 10 ms (64.29%) | Memory: 17.7 MB (70.38%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -11,11 +11,15 @@
  */
 class Solution {
 public:
+    map<pair<int,int>, vector<TreeNode*>> mp;
     vector<TreeNode*> helper(int start, int end){
         vector<TreeNode*> v;
         if(start>end){
             v.push_back(nullptr);
-            return v;
+            return mp[{start, end}] = v;
+        }
+        if(mp[{start, end}].size()!=0){
+            return mp[{start, end}];
         }
         for(int i=start; i<=end;i++){
             for(auto it:helper(start, i-1)){
@@ -27,7 +31,7 @@ public:
                 }
             }
         }
-        return v;
+        return mp[{start, end}] = v;
     }
     vector<TreeNode*> generateTrees(int n) {
         if(n==0) return {};
